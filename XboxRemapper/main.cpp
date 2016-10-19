6bb2c17668e805641da8c993e197d9cf4f6ca4c6
Reed Jones <develop@reedjones.com>
2016-10-18 23:14:34 -0600

    formatting, comments

--- XboxRemapper/main.cpp
+++ XboxRemapper/main.cpp
@@ -20,11 +20,9 @@
 int main()
 {
 	// get the xinput dll, for older computers (such as windows 7), try xinput1_3.dll instead
-	// Note... this breaks battery code
 	HINSTANCE xinputDll = LoadLibrary("xinput1_4.dll");
 	remapper::wrapper::XInputGetStateEx XInputGetState = (remapper::wrapper::XInputGetStateEx)GetProcAddress(xinputDll, (LPCSTR)100);
 	remapper::wrapper::XInputPowerOffController_t realXInputPowerOffController = (remapper::wrapper::XInputPowerOffController_t)GetProcAddress(xinputDll, (LPCSTR)103);
-
 
 	XINPUT_STATE state;										// Stores the gamepad state of all players
 	ZeroMemory(&state, sizeof(XINPUT_STATE));
@@ -34,22 +32,20 @@ int main()
 	bool powerOff[4],										// if guide button is to become power button for controller
 		wasPressedPrev[4] = { false,false,false,false };	// no key has been pressed yet
 
-	{
-		// load ini config
-		remapper::iniReader::INIFile ini;
-		ini.loadFile("button.ini");
+	// load ini config
+	remapper::iniReader::INIFile ini;
+	ini.loadFile("button.ini");
 
-		// load defaults if config is messed up. Last digit is the defaults.
-		for (int i = 0; i < 4; ++i)
-		{
-			whichKey[i] = ini.getInteger("config", "key_player" + std::to_string(i + 1), 27);		// defaults to escape
-			powerOff[i] = ini.getInteger("config", "off_player" + std::to_string(i + 1), 0);			// defaults to not poweroff
-		}
+	// load defaults if config is messed up. Last digit is the defaults.
+	for (int i = 0; i < 4; ++i)
+	{
+		std::string player = "_player" + std::to_string(i + 1);				// player1, player2 etc...
+		whichKey[i] = ini.getInteger("config", "key" + player, 27);			// defaults to escape
+		powerOff[i] = ini.getInteger("config", "off" + player, 0);			// defaults to not poweroff
 	}
 
-	//XINPUT_BATTERY_INFORMATION XBatt;
-
 	printf("Congratulations, the code compiled!\nTry pressing a button (probably the guide at this point)\n\n");
+
 	for (;;)
 	{
 		for (DWORD i = 0; i < XUSER_MAX_COUNT; i++)
@@ -59,13 +55,11 @@ int main()
 			{
 				//guideButtonPressed(state, wasPressedPrev[i], powerOff[i], whichKey[i], delays[i], durations[i], shouldHold[i]);
 				////// Diagnose which button was pressed, and what its mapped too
-				//if (state.Gamepad.wButtons != 0)
-				//{
-
-				//	//printf("%d ", (byte)XBatt.BatteryLevel);
-				//	printf("Player %d pressed %d on the gamepad.\n", i + 1, state.Gamepad);
-				//	printf("This is mapped to %d on the keyboard\n\n", whichKey[i]);
-				//}
+				if (state.Gamepad.wButtons != 0)
+				{
+					printf("Player %d pressed %d on the gamepad.\n", i + 1, state.Gamepad);
+					printf("This is mapped to %d on the keyboard\n\n", whichKey[i]);
+				}
 
 				// compare with guide button, if true, check if its alread being held
 				if (state.Gamepad.wButtons & remapper::wrapper::GUIDE_BUTTON)
@@ -105,5 +99,6 @@ int main()
 
 	// free library, safely exit.
 	FreeLibrary(xinputDll);
+	xinputDll = NULL;
 	return 0;
 }
